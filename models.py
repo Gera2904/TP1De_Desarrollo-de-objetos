347713931c17b588883d8c0e96015b9d02674064
gtera <gteramchourio@gmail.com>
2025-06-11 19:06:08 -0300

    Agregando nuevos archivos al proyecto

---
+++ models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Perro(models.Model):
+nombre = models.CharField(max_length=100)
+    tamaño = models.CharField(max_length=50)
+    raza = models.CharField(max_length=50)
+    peso = models.DecimalField(max_digits=5, decimal_places=2)
+    estado_salud = models.CharField(max_length=100)
+    vacunado = models.BooleanField(default=False)
+    estado = models.CharField(max_length=50)
+
+
+class UsuarioAdoptante(models.Model):
+    nombre = models.CharField(max_length=100)
+    dni = models.CharField(max_length=20, unique=True)
+    email = models.EmailField()
+    preferencias = models.TextField()
+    historial_adopciones = models.ManyToManyField(Perro, blank=True)
+
