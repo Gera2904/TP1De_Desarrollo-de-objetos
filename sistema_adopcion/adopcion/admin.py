656ebd863457e15740742d1c201a2ff055bccee3
gtera <gteramchourio@gmail.com>
2025-06-19 00:33:22 -0300

    Registro modelo Preferencias en el admin

--- sistema_adopcion/adopcion/admin.py
+++ sistema_adopcion/adopcion/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Perro, UsuarioAdoptante
+from .models import Perro, UsuarioAdoptante, Preferencias
 
 admin.site.register(Perro)
 admin.site.register(UsuarioAdoptante)
-
+admin.site.register(Preferencias)
